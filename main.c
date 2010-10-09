33a7cd59d9bdc8c89959eaa72e53b790404fa417
Trygve Vea <trygve.vea@gmail.com>
2010-10-10 00:15:22 +0200

    Indentation fixes.

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 struct grid {
-	int w, h;
-	int *ar;
-	int *path;
+    int w, h;
+    int *ar;
+    int *path;
 }grid;
 
 int checked = 0;
@@ -20,24 +20,24 @@ int *cursor_d;
 void checkfield(int cursor, int nextval)
 {
 // This was a pure test of how much time was wasted walking too far. The answer: a lot.
-//	if ( nextval > 42 ) return;
-	if ( grid.path[cursor] > 0 ) wasnt0++;
-	checked++;
+//    if ( nextval > 42 ) return;
+    if ( grid.path[cursor] > 0 ) wasnt0++;
+    checked++;
     grid.path[cursor] = nextval;
-	switch(grid.ar[cursor])
-	{
-		case 0: case 255: break;
-		case 1: grid.path[cursor] = -1; return; break;
-		default: return;
-	}
-	if ( (cursor+1) % grid.w != 0 && cursor+1 < grid.w*grid.h && marked[cursor+1] == 0 )
-		if ( grid.path[cursor+1] == 0 || grid.path[cursor+1] > (nextval+1) ) {
+    switch(grid.ar[cursor])
+    {
+        case 0: case 255: break;
+        case 1: grid.path[cursor] = -1; return; break;
+        default: return;
+    }
+    if ( (cursor+1) % grid.w != 0 && cursor+1 < grid.w*grid.h && marked[cursor+1] == 0 )
+        if ( grid.path[cursor+1] == 0 || grid.path[cursor+1] > (nextval+1) ) {
             cursor_n[0] = cursor+1;
             cursor_n[1] = nextval+1;
             marked[cursor+1]++;
             cursor_n += 2;
         }
-//			checkfield(cursor+1, nextval+1);
+//            checkfield(cursor+1, nextval+1);
     if ( (cursor) % grid.w != 0 && cursor-1 >= 0 && marked[cursor-1] == 0 )
         if ( grid.path[cursor-1] == 0 || grid.path[cursor-1] > (nextval+1) ) {
             cursor_n[0] = cursor-1;
@@ -45,7 +45,7 @@ void checkfield(int cursor, int nextval)
             marked[cursor-1]++;
             cursor_n += 2;
         }
-//			checkfield(cursor-1, nextval+1);
+//            checkfield(cursor-1, nextval+1);
     if ( (cursor+grid.w) < grid.w*grid.h && marked[cursor+grid.w] == 0 )
         if ( grid.path[cursor+grid.w] == 0 || grid.path[cursor+grid.w] > (nextval+1) ) {
             cursor_n[0] = cursor+grid.w;
@@ -53,7 +53,7 @@ void checkfield(int cursor, int nextval)
             marked[cursor+grid.w]++;
             cursor_n += 2;
         }
-//			checkfield(cursor+grid.w, nextval+1);
+//            checkfield(cursor+grid.w, nextval+1);
     if ( (cursor-grid.w) >= 0 && marked[cursor-grid.w] == 0 )
         if ( grid.path[cursor-grid.w] == 0 || grid.path[cursor-grid.w] > (nextval+1) ) {
             cursor_n[0] = cursor-grid.w;
@@ -61,34 +61,34 @@ void checkfield(int cursor, int nextval)
             marked[cursor-grid.w]++;
             cursor_n += 2;
         }
-//			checkfield(cursor-grid.w, nextval+1);
-	checkedcomplete++;
+//            checkfield(cursor-grid.w, nextval+1);
+    checkedcomplete++;
 }
 
 int main(int argc, char *argv[])
 {
-	int i, o, cursor = 0;
-	printf("How large is your grid (WxH)? ");
-	scanf("%dx%d",&grid.w, &grid.h);
+    int i, o, cursor = 0;
+    printf("How large is your grid (WxH)? ");
+    scanf("%dx%d",&grid.w, &grid.h);
 
-	grid.ar = malloc(sizeof(int)*grid.w*grid.h);
-	grid.path = malloc(sizeof(int)*grid.w*grid.h);
+    grid.ar = malloc(sizeof(int)*grid.w*grid.h);
+    grid.path = malloc(sizeof(int)*grid.w*grid.h);
     list = malloc(sizeof(int)*grid.w*grid.h*2);
     marked = malloc(sizeof(int)*grid.w*grid.h);
     cursor_n = cursor_d = list;
-	memset(grid.path, '\0', sizeof(int)*grid.w*grid.h);
+    memset(grid.path, '\0', sizeof(int)*grid.w*grid.h);
     memset(list, '\0', sizeof(int)*grid.w*grid.h*2);
     memset(marked, '\0', sizeof(int)*grid.w*grid.h);
 
 
-	if ( !grid.ar || !grid.path ) {
-		puts("Not enough memory");
-		return -1;
-	}
+    if ( !grid.ar || !grid.path ) {
+        puts("Not enough memory");
+        return -1;
+    }
 
-	for(i=0;i<grid.w*grid.h;i++) {
-		if (!scanf("%d",&(grid.ar[i]))) puts("wat!");
-	}
+    for(i=0;i<grid.w*grid.h;i++) {
+        if (!scanf("%d",&(grid.ar[i]))) puts("wat!");
+    }
 
 //    for(o=0;o<10000;o++)
 //    {
@@ -97,33 +97,33 @@ int main(int argc, char *argv[])
         memset(list, '\0', sizeof(int)*grid.w*grid.h*2);
         memset(marked, '\0', sizeof(int)*grid.w*grid.h);
 
-    	for(i=0;i<grid.w*grid.h;i++)
-    	{
-//    		if ( i % grid.w == 0 ) printf("\n");
-    		switch(grid.ar[i]) {
-/*    			case 0:		printf("   "); break;
-    			case 1:		printf("###"); break;
-    			case 254:	printf("SSS"); break;*/
-    			case 255:	/*printf("EEE"); */cursor = i;
+        for(i=0;i<grid.w*grid.h;i++)
+        {
+//            if ( i % grid.w == 0 ) printf("\n");
+            switch(grid.ar[i]) {
+/*                case 0:        printf("   "); break;
+                case 1:        printf("###"); break;
+                case 254:    printf("SSS"); break;*/
+                case 255:    /*printf("EEE"); */cursor = i;
                     cursor_n[0] = i;
                     cursor_n[1] = 1;
                     marked[i]++;
                     cursor_n += 2;
                 break;
-    		}
-    	}
-//    	printf("\n");
+            }
+        }
+//        printf("\n");
 
-//	for(i=0;i<1000;i++)
-//	{
-//		memset(grid.path, '\0', sizeof(int)*grid.w*grid.h);
+//    for(i=0;i<1000;i++)
+//    {
+//        memset(grid.path, '\0', sizeof(int)*grid.w*grid.h);
         while ( marked[cursor_d[0]] ) {
 //            printf("checkfield(%d,%d)\n",cursor_d[0],cursor_d[1]);
-    		checkfield(cursor_d[0],cursor_d[1]);
+            checkfield(cursor_d[0],cursor_d[1]);
             cursor_d += 2;
         }
-//	}
-	
+//    }
+    
     for(i=0;i<grid.w*grid.h;i++)
     {
         if ( i % grid.w == 0 ) printf("\n");
@@ -135,13 +135,13 @@ int main(int argc, char *argv[])
 
         }
     }
-	printf("\n\n");
+    printf("\n\n");
 
-	printf("checked: %d\ncheckedcomplete: %d\nwasnt0: %d\n", checked, checkedcomplete,wasnt0);
+    printf("checked: %d\ncheckedcomplete: %d\nwasnt0: %d\n", checked, checkedcomplete,wasnt0);
 
-	free(grid.ar);
-	free(grid.path);
+    free(grid.ar);
+    free(grid.path);
     free(list);
     free(marked);
-	return 0;
+    return 0;
 }

b914c8bb0b5bfdbb5fa81d00b480892a6ad407d8
Trygve Vea <trygve.vea@gmail.com>
2010-10-09 23:56:53 +0200

    Significant optimizations :)

--- main.c
+++ main.c
@@ -11,6 +11,11 @@ struct grid {
 int checked = 0;
 int checkedcomplete = 0;
 int wasnt0 = 0;
+int *list;
+int *marked;
+int *cursor_n;
+int *cursor_d;
+
 
 void checkfield(int cursor, int nextval)
 {
@@ -25,30 +30,56 @@ void checkfield(int cursor, int nextval)
 		case 1: grid.path[cursor] = -1; return; break;
 		default: return;
 	}
-	if ( (cursor+1) % grid.w != 0 && cursor+1 < grid.w*grid.h )
-		if ( grid.path[cursor+1] == 0 || grid.path[cursor+1] > (nextval+1) )
-			checkfield(cursor+1, nextval+1);
-    if ( (cursor) % grid.w != 0 && cursor-1 >= 0 )
-        if ( grid.path[cursor-1] == 0 || grid.path[cursor-1] > (nextval+1) )
-			checkfield(cursor-1, nextval+1);
-    if ( (cursor+grid.w) < grid.w*grid.h )
-        if ( grid.path[cursor+grid.w] == 0 || grid.path[cursor+grid.w] > (nextval+1) )
-			checkfield(cursor+grid.w, nextval+1);
-    if ( (cursor-grid.w) >= 0 )
-        if ( grid.path[cursor-grid.w] == 0 || grid.path[cursor-grid.w] > (nextval+1) )
-			checkfield(cursor-grid.w, nextval+1);
+	if ( (cursor+1) % grid.w != 0 && cursor+1 < grid.w*grid.h && marked[cursor+1] == 0 )
+		if ( grid.path[cursor+1] == 0 || grid.path[cursor+1] > (nextval+1) ) {
+            cursor_n[0] = cursor+1;
+            cursor_n[1] = nextval+1;
+            marked[cursor+1]++;
+            cursor_n += 2;
+        }
+//			checkfield(cursor+1, nextval+1);
+    if ( (cursor) % grid.w != 0 && cursor-1 >= 0 && marked[cursor-1] == 0 )
+        if ( grid.path[cursor-1] == 0 || grid.path[cursor-1] > (nextval+1) ) {
+            cursor_n[0] = cursor-1;
+            cursor_n[1] = nextval+1;
+            marked[cursor-1]++;
+            cursor_n += 2;
+        }
+//			checkfield(cursor-1, nextval+1);
+    if ( (cursor+grid.w) < grid.w*grid.h && marked[cursor+grid.w] == 0 )
+        if ( grid.path[cursor+grid.w] == 0 || grid.path[cursor+grid.w] > (nextval+1) ) {
+            cursor_n[0] = cursor+grid.w;
+            cursor_n[1] = nextval+1;
+            marked[cursor+grid.w]++;
+            cursor_n += 2;
+        }
+//			checkfield(cursor+grid.w, nextval+1);
+    if ( (cursor-grid.w) >= 0 && marked[cursor-grid.w] == 0 )
+        if ( grid.path[cursor-grid.w] == 0 || grid.path[cursor-grid.w] > (nextval+1) ) {
+            cursor_n[0] = cursor-grid.w;
+            cursor_n[1] = nextval+1;
+            marked[cursor-grid.w]++;
+            cursor_n += 2;
+        }
+//			checkfield(cursor-grid.w, nextval+1);
 	checkedcomplete++;
 }
 
 int main(int argc, char *argv[])
 {
-	int i, cursor = 0;
+	int i, o, cursor = 0;
 	printf("How large is your grid (WxH)? ");
 	scanf("%dx%d",&grid.w, &grid.h);
 
 	grid.ar = malloc(sizeof(int)*grid.w*grid.h);
 	grid.path = malloc(sizeof(int)*grid.w*grid.h);
+    list = malloc(sizeof(int)*grid.w*grid.h*2);
+    marked = malloc(sizeof(int)*grid.w*grid.h);
+    cursor_n = cursor_d = list;
 	memset(grid.path, '\0', sizeof(int)*grid.w*grid.h);
+    memset(list, '\0', sizeof(int)*grid.w*grid.h*2);
+    memset(marked, '\0', sizeof(int)*grid.w*grid.h);
+
 
 	if ( !grid.ar || !grid.path ) {
 		puts("Not enough memory");
@@ -59,22 +90,38 @@ int main(int argc, char *argv[])
 		if (!scanf("%d",&(grid.ar[i]))) puts("wat!");
 	}
 
-	for(i=0;i<grid.w*grid.h;i++)
-	{
-		if ( i % grid.w == 0 ) printf("\n");
-		switch(grid.ar[i]) {
-			case 0:		printf("   "); break;
-			case 1:		printf("###"); break;
-			case 254:	printf("SSS"); break;
-			case 255:	printf("EEE"); cursor = i; break;
-		}
-	}
-	printf("\n");
+    for(o=0;o<60000;o++)
+    {
+        cursor_n = cursor_d = list;
+        memset(grid.path, '\0', sizeof(int)*grid.w*grid.h);
+        memset(list, '\0', sizeof(int)*grid.w*grid.h*2);
+        memset(marked, '\0', sizeof(int)*grid.w*grid.h);
 
-	for(i=0;i<1000;i++)
-	{
-		memset(grid.path, '\0', sizeof(int)*grid.w*grid.h);
-		checkfield(cursor,1);
+    	for(i=0;i<grid.w*grid.h;i++)
+    	{
+//    		if ( i % grid.w == 0 ) printf("\n");
+    		switch(grid.ar[i]) {
+/*    			case 0:		printf("   "); break;
+    			case 1:		printf("###"); break;
+    			case 254:	printf("SSS"); break;*/
+    			case 255:	/*printf("EEE"); */cursor = i;
+                    cursor_n[0] = i;
+                    cursor_n[1] = 1;
+                    marked[i]++;
+                    cursor_n += 2;
+                break;
+    		}
+    	}
+//    	printf("\n");
+
+//	for(i=0;i<1000;i++)
+//	{
+//		memset(grid.path, '\0', sizeof(int)*grid.w*grid.h);
+        while ( marked[cursor_d[0]] ) {
+//            printf("checkfield(%d,%d)\n",cursor_d[0],cursor_d[1]);
+    		checkfield(cursor_d[0],cursor_d[1]);
+            cursor_d += 2;
+        }
 	}
 	
     for(i=0;i<grid.w*grid.h;i++)
